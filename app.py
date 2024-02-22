232e0279ad65250e0e041b943b30ef99888ed14c
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2024-02-22 11:29:24 +0900

    first commit

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index():
     return {"hello": "world"}
 
 
+# Function to return photo metadata for designated album from DynamoDB
 @app.route("/album/{category}", cors=True)
 def read_metadata(category):
     print("category:", category)
@@ -38,24 +39,3 @@ def read_metadata(category):
     print(converted_items[0])
 
     return {"items": converted_items}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
